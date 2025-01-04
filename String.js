482322d365ce21a1a78d43cf1652f60b1324dfd2
SanskarVishwakarma <89730080+SanskarVishwakarma@users.noreply.github.com>
2025-01-04 16:51:48 +0000

    changes added

---
+++ String.js
@@ -0,0 +1,25 @@
+const newStringOne = "   hitesh  "
+console.log(newStringOne);
+console.log(newStringOne.trim);
+
+const gameName= 'hitesh-choudhary-sharma'
+
+//replace
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+//console.log(url.replace('%20','-'));
+//console.log(url.includes('hitesh')); //true
+
+//console.log(gameName.split('-'));
+
+//charAt
+const string1 = "Sanskar"
+//console.log(string1.charAt(3));
+
+const string2 = new String("2+2")
+console.log(eval(string2.valueOf()));
+console.log(eval(string2.valueOf()));
+console.log(string1.valueOf());
+
+console.log(string1.indexOf("N"));
+
---
+++ numbersMaths.js
@@ -0,0 +1,29 @@
+const score = 400
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance);
+
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(3));
+
+//toPrecision
+
+const otherNumber = 123.5000
+
+//    console.log(otherNumber.toPrecision(3));
+
+const hundreds = 1000000
+
+//console.log(hundreds.toLocaleString('en-IN'));
+
+// ================Maths==========================
+
+console.log(Math);
+
+console.log((Math.random()*1) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
